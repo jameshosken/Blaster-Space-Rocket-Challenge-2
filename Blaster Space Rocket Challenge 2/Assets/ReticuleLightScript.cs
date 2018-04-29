4db466f095d8a48cdce3925c1d73dd27a0186ce5
James Hosken <jameswfhosken@gmail.com>
2018-04-29 13:56:58 +0100

    Tweak follow movement

---
+++ Blaster Space Rocket Challenge 2/Assets/ReticuleLightScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticuleLightScript : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] float lag = 0.5f;
+
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        transform.position = Vector3.Lerp(transform.position, target.position, lag);
+        
+        //transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, zDistance);
+        //transform.localPosition.Set(transform.localPosition.x, transform.localPosition.y, zDistance);
+	}
+}
