ec2b469a88568300a9c06c9ad3d4950fe0ab4476
James Hosken <jameswfhosken@gmail.com>
2018-04-29 12:16:53 +0100

    Add particle cannons

--- Blaster Space Rocket Challenge 2/Assets/AlignToTrajectory.cs
+++ Blaster Space Rocket Challenge 2/Assets/AlignToTrajectory.cs
@@ -2,28 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AlignToTrajectory : MonoBehaviour {
+public class AlignToTrajectory : MonoBehaviour
+{
 
-    [SerializeField] float lag = 0.5f;
+    [SerializeField] float trajectoryLag = 0.1f;
+    [SerializeField] float turnLag = 0.5f;
+
     Vector3 previousPosition = new Vector3(0, 0, 0);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public Vector3 GetOrientationFromTrajectory()
+    {
         Vector3 orientation = this.transform.position - previousPosition;
-
-        
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(orientation), lag * Time.deltaTime);
-
-        //Vector3 goal = this.transform.position + orientation;
-
-        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, goal.rotation, lag);
-
         previousPosition = this.transform.position;
-	}
+        return orientation;
+    }
+
 }
+
+
--- Blaster Space Rocket Challenge 2/Assets/PlayerMovement.cs
+++ Blaster Space Rocket Challenge 2/Assets/PlayerMovement.cs
@@ -8,39 +8,117 @@ public class PlayerMovement : MonoBehaviour {
     [Tooltip("In ms^-1")][SerializeField]float speed = 1f;
 
     FollowTargetPosition followTarget;
+    AlignToTrajectory alignTrajectory;                  //Rename to rotationScript
 
     [SerializeField] Transform target;
+
     [SerializeField] float xClamp = 5f;
     [SerializeField] float yClamp = 5f;
+
+    [SerializeField] float positionPitchFactor = 20f;
+    [SerializeField] float controlPitchFactor = -20f;
+
+    [SerializeField] float positionYawFactor = 20f;
+    [SerializeField] float controlYawFactor = -20f;
+    //[SerializeField] float positionPitchFactor = 1f;
+
 
 
     private void Start()
     {
         followTarget = GetComponent<FollowTargetPosition>();
+        alignTrajectory = GetComponent<AlignToTrajectory>();
     }
 
     void Update () {
 
-        //don't overwrite original target
-        //Transform newTarget = target;
+        float[] throwXY = GetInputThrow();
+        float[] offset  = GetOffsetXY(throwXY[0], throwXY[1]);
+        float[] clamped = GetClampedXY(offset[0], offset[1]);
 
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = horizontalThrow * speed * Time.deltaTime;
-        float rawNewXPos = target.localPosition.x + xOffset;
-        float clampedXOffset = Mathf.Clamp(rawNewXPos, -xClamp, xClamp);
-
-        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        float yOffset = verticalThrow * speed * Time.deltaTime;
-        float rawNewYPos = target.localPosition.y + yOffset;
-        float clampedYOffset = Mathf.Clamp(rawNewYPos, -yClamp, yClamp);
+        ProcessPosition(clamped[0], clamped[1]);
         
-
-        target.localPosition = new Vector3(clampedXOffset, clampedYOffset, 0);
-
-
-        //newTarget.Translate(translateVector, Space.Self);
-
-        followTarget.ApplyFollowTarget(target);
+        ProcessRotation(clamped, throwXY);
 
 	}
+
+    float[] GetInputThrow()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float[] throwXY = new float[2];
+        throwXY[0] = xThrow;
+        throwXY[1] = yThrow;
+        return throwXY;
+    }
+
+    float[] GetOffsetXY(float xThrow, float yThrow) { 
+
+        float xOffset = xThrow * speed * Time.deltaTime;
+        
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float[] offset = new float[2];
+        offset[0] = xOffset;
+        offset[1] = yOffset;
+        return offset;
+    }
+
+    float [] GetClampedXY(float xoff, float yoff)
+    {
+        
+        float rawNewXPos = target.localPosition.x + xoff;
+        float clampedXOffset = Mathf.Clamp(rawNewXPos, -xClamp, xClamp);
+
+        
+        float rawNewYPos = target.localPosition.y + yoff;
+        float clampedYOffset = Mathf.Clamp(rawNewYPos, -yClamp, yClamp);
+
+        float[] clamped = new float[2];
+        clamped[0] = clampedXOffset;
+        clamped[1] = clampedYOffset;
+        return  clamped;
+    }
+
+    void ProcessPosition(float x, float y)
+    {
+
+        target.localPosition = new Vector3(x, y, 0);
+        followTarget.ApplyFollowTarget(target);
+
+    }
+
+    /// <summary>
+    /// ROTATION
+    /// </summary>
+    
+    void ProcessRotation(float[] clamped, float[] throwXY)
+    {
+        Vector3 orientation = alignTrajectory.GetOrientationFromTrajectory();
+        transform.LookAt(target);
+
+        ApplyTurnRotationFromLocalMovement(clamped, throwXY);
+
+    }
+
+   
+    void ApplyTurnRotationFromLocalMovement(float[] localXY, float[] throwXY)
+    {
+        float pitchDueToPosition = localXY[1] * positionPitchFactor;
+        float pitchDueToControlThrow = throwXY[1] * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yawDueToPosition = localXY[0] * positionYawFactor;
+        float yawDueToControlThrow = throwXY[0] * controlYawFactor;
+        float yaw = yawDueToPosition + yawDueToControlThrow;
+
+        Vector3 turnAngles = new Vector3(pitch, yaw, 0);
+
+        transform.Rotate(turnAngles, Space.Self);
+
+    }
+
+
 }
+
+
