cbdcb575db2d143ddba69d225f78a67fc1a750da
James Hosken <jameswfhosken@gmail.com>
2018-04-30 19:40:23 +0100

    Enhance Space Station

---
+++ Blaster Space Rocket Challenge 2/Assets/RotationScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+
+    [SerializeField] Vector3 axis;
+    [SerializeField] bool randomStart = true;
+
+
+    private void Start()
+    {
+        if (randomStart)
+        {
+            transform.Rotate( axis * Random.Range(0, 2* Mathf.PI),Space.Self);
+        }
+    }
+    void Update () {
+        transform.Rotate(axis * Time.deltaTime, Space.Self);
+	}
+}
